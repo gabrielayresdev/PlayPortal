c01bc41103f5a192431f08745b58c2e8e735c1c0
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-01 19:44:54 -0300

    Refactor: Criação do custom hook useFetch.

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const useFetch = () => {
+  const [error, setError] = React.useState(null);
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    let json;
+    let response;
+    try {
+      setError(false);
+      setLoading(true);
+
+      response = await fetch(url, options);
+      json = await response.json();
+
+      // Se a response não estiver no range de 200 a 299, um erro será gerado e capturado pelo catch
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      // Limpa o valor de json para não ser usado como data
+      json = null;
+      setError(err.message);
+    } finally {
+      setLoading(false);
+      setData(json);
+      return { json, response };
+    }
+  }, []);
+
+  return { error, data, loading, request };
+};
+
+export default useFetch;
