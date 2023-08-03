7d91d5f68c2e454d685488cd699057efeb96ab33
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-02 21:21:14 -0300

    Feat: Adição da funcionalidade de carregar mais filmes.

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -2,30 +2,43 @@ import React from "react";
 import useFetch from "../../hooks/useFetch";
 import Thumbnail from "./Thumbnail";
 import styles from "./Home.module.css";
+import Button from "../Button";
 
 const Home = () => {
   const { error, loading, data, request } = useFetch();
+  const [movies, setMovies] = React.useState([]);
+  const [page, setPage] = React.useState(1);
 
   React.useEffect(() => {
-    const url =
-      "https://api.themoviedb.org/3/discover/movie?api_key=03de48f66303824c443b36741744feac&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=1000&language=pt-BR";
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=03de48f66303824c443b36741744feac&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=1000&language=pt-BR&page=${page}`;
 
     request(url);
-  }, [request]);
+  }, [request, page]);
+
+  React.useEffect(() => {
+    if (data) setMovies((movies) => [...movies, ...data.results]);
+  }, [data]);
 
   if (loading) return <div>Carregando</div>;
   if (error) return <div>{error}</div>;
-  if (data)
+  if (movies.length > 0)
     return (
-      <div>
-        <h1 className={styles.title}>Mais bem avaliados</h1>
+      <>
+        <div>
+          <h1 className={styles.title}>Mais bem avaliados</h1>
 
-        <div className={styles.main}>
-          {data.results.map((item) => {
-            return <Thumbnail key={item.id} data={item} type={"movie"} />;
-          })}
+          <div className={styles.main}>
+            {movies.map((item) => {
+              return <Thumbnail key={item.id} data={item} type={"movie"} />;
+            })}
+          </div>
         </div>
-      </div>
+        <div className={styles.button_container}>
+          <Button handleClick={() => setPage((page) => page + 1)}>
+            Carregar mais
+          </Button>
+        </div>
+      </>
     );
 };
 
--- src/components/main/Thumbnail.jsx
+++ src/components/main/Thumbnail.jsx
@@ -2,7 +2,6 @@ import Format from "../../Classes/Format";
 import styles from "./Thumbnail.module.css";
 
 const Thumbnail = ({ data, type }) => {
-  console.log(data);
   return (
     <div className={styles.thumbnail}>
       <div className={styles.thumbnail_image}>
@@ -20,7 +19,7 @@ const Thumbnail = ({ data, type }) => {
           <path
             d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
             stroke="#FFF"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             fill="none"
           />
         </svg>
