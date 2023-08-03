37de456f02bae8b144e2b591e2767ed492708d3c
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-08-02 23:26:21 -0300

    Fix(homepage): Resolvido bug em que ao clicar no botão de carregar mais filmes, o scroll voltava para o começo da página.

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,45 +1,31 @@
 import React from "react";
-import useFetch from "../../hooks/useFetch";
-import Thumbnail from "./Thumbnail";
 import styles from "./Home.module.css";
 import Button from "../Button";
+import ShowsFeed from "./ShowsFeed";
 
 const Home = () => {
-  const { error, loading, data, request } = useFetch();
-  const [movies, setMovies] = React.useState([]);
-  const [page, setPage] = React.useState(1);
+  const [pages, setPages] = React.useState([1]);
 
-  React.useEffect(() => {
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=03de48f66303824c443b36741744feac&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=1000&language=pt-BR&page=${page}`;
+  return (
+    <>
+      <div>
+        <h1 className={styles.title}>Mais bem avaliados</h1>
 
-    request(url);
-  }, [request, page]);
-
-  React.useEffect(() => {
-    if (data) setMovies((movies) => [...movies, ...data.results]);
-  }, [data]);
-
-  if (loading) return <div>Carregando</div>;
-  if (error) return <div>{error}</div>;
-  if (movies.length > 0)
-    return (
-      <>
         <div>
-          <h1 className={styles.title}>Mais bem avaliados</h1>
-
-          <div className={styles.main}>
-            {movies.map((item) => {
-              return <Thumbnail key={item.id} data={item} type={"movie"} />;
-            })}
-          </div>
+          {pages.map((page) => {
+            return <ShowsFeed key={page} page={page} />;
+          })}
         </div>
-        <div className={styles.button_container}>
-          <Button handleClick={() => setPage((page) => page + 1)}>
-            Carregar mais
-          </Button>
-        </div>
-      </>
-    );
+      </div>
+      <div className={styles.button_container}>
+        <Button
+          handleClick={() => setPages((page) => [...page, page.length + 1])}
+        >
+          Carregar mais
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/main/ShowsFeed.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./ShowsFeed.module.css";
+import Thumbnail from "./Thumbnail";
+import useFetch from "../../hooks/useFetch";
+
+const ShowsFeed = ({ page }) => {
+  const { error, loading, data, request } = useFetch();
+
+  React.useEffect(() => {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=03de48f66303824c443b36741744feac&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=1000&language=pt-BR&page=${page}`;
+
+    request(url);
+  }, [request, page]);
+
+  if (loading) return <div>Carregando</div>;
+  if (error) return <div>{error}</div>;
+  if (data)
+    return (
+      <div>
+        <div className={styles.main}>
+          {data.results.map((item, index) => {
+            return <Thumbnail key={index} data={item} type={"movie"} />;
+          })}
+        </div>
+      </div>
+    );
+  else return null;
+};
+
+export default ShowsFeed;
